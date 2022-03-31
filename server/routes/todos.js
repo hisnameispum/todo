1016c3b259fc329598756607469181955b2cee01
sirasit <sirasit@siruxsolutions.com>
2022-03-31 00:53:07 -0700

    Tested with database

--- server/routes/todos.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { createTodos, readTodos } from '../controller/todos.js';
-const router = express.Router();
-router.get('/', readTodos);
-router.post('/', createTodos);
-export default router;
